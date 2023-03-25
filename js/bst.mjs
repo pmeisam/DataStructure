1b6a66e427d2b02b31e49a2fdcbd4920f9516160
Meisam Poorzand <pmeisam@ymail.com>
2023-03-25 16:10:41 -0700

    updates bst in js

--- js/bst.mjs
+++ js/bst.mjs
@@ -9,45 +9,40 @@ class BinarySearchTree {
   insert(value) {
     let node = new Node(value);
     if (!this.root) {
-      this.root = node;
-      return;
+        this.root = node;
+        return;
     }
 
-    let current = this.root;
+    let curr = this.root;
 
-    while (current) {
-      if (current.value <= value) {
-        // Right
-        if (current.right === null) {
-          current.right = node;
-          return this;
+    while (curr) {
+        if (value < curr.value) { // Left
+            if (!curr.left) {
+                curr.left = node;
+                return this;
+            }
+            curr = curr.left;
+        } else { // Right
+            if (!curr.right) {
+                curr.right = node;
+                return this;
+            }
+            curr = curr.right;
         }
-        current = current.right;
-      } else {
-        // Left
-        if (current.left === null) {
-          current.left = node;
-          return this;
-        }
-        current = current.left;
-      }
     }
   }
 
 
   lookup(value) {
-    if (!this.root) return false;
-
-    let current = this.root;
-    while (current) {
-      if (current.value === value) {
-        return current;
-      }
-      if (current.value < value) {
-        current = current.right;
-      } else {
-        current = current.left;
-      }
+    let root = this.root;
+    while (root) {
+        if (root.value === value) {
+            return root;
+        } if (value < root.value) {
+            root = root.left;
+        } else {
+            root = root.right;
+        }
     }
     return false;
   }
@@ -112,9 +107,31 @@ class BinarySearchTree {
 
 let bst = new BinarySearchTree();
 
-bst.insert(10);
-bst.insert(5);
+bst.insert(20);
+bst.insert(4);
+bst.insert(1);
+bst.insert(6);
 bst.insert(15);
+bst.insert(170);
+bst.insert(160);
+bst.insert(165);
+bst.insert(150);
+bst.insert(175);
+bst.insert(172);
+bst.insert(180);
 
 bst.print();
 
+console.log(JSON.stringify(traverse(bst.root)));
+bst.remove(9);
+console.log(JSON.stringify(traverse(bst.root)));
+
+console.log(bst.lookup(175));
+console.log(bst.lookup(19));
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
